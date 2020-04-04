2d2086081c55ae06fc103b21302c92f330222205
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-04-04 17:42:43 +0200

    Updates

---
+++ precache-manifest.3f8c8ee83b958e96290e1927db7a0fa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9511407a75f24bf2c058b5b9aa2521d9",
+    "url": "/crypto_history/index.html"
+  },
+  {
+    "revision": "4e7176178ccb3b6015ff",
+    "url": "/crypto_history/static/css/main.a58b2782.chunk.css"
+  },
+  {
+    "revision": "b9ca21f80dc1373a31c9",
+    "url": "/crypto_history/static/js/2.b6f3a4ae.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/crypto_history/static/js/2.b6f3a4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7176178ccb3b6015ff",
+    "url": "/crypto_history/static/js/main.2aaa0ba9.chunk.js"
+  },
+  {
+    "revision": "b889ab48804c03c76369",
+    "url": "/crypto_history/static/js/runtime-main.a65eeb7f.js"
+  },
+  {
+    "revision": "182e899286c96546a1c7925ddb1dd5cc",
+    "url": "/crypto_history/static/media/bitcoin-brands.182e8992.svg"
+  }
+]);
